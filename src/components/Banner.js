c466346d39238bf8f0463f0665ab3fd4e0fafd48
graycummins <>
2023-08-12 09:28:10 -0500

    All files are up and running! All done!:)

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,3 +1,9 @@
+/* Created by: Grayson Cummins
+   Date: 12 August 2023
+
+   Banner will display 'Orbit Report' with a blue background 
+   Banner will also provide the user with the data on how to use the app
+*/
 import './styling.css';
 
 function Banner(){
