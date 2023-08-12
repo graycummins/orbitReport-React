08da7ea990ae8da6732d6e658d98452f6234cb44
graycummins <>
2023-08-12 08:36:23 -0500

    almost done with part 5

---
+++ src/components/Banner.js
@@ -0,0 +1,13 @@
+import './styling.css';
+
+function Banner(){
+
+   return(
+    <>
+    <header>Orbit Report</header>
+    <p>Click on the buttons to see the satellites in the orbit type</p>
+    </>
+   );
+}
+
+export default Banner;
